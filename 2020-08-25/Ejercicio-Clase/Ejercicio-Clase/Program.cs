c05813dee489587bbc132d7aec0ac744b3732bc6
MDarrell <mau_eo@hotmail.com>
2020-08-25 23:03:55 -0300

    ejercicio

---
+++ 2020-08-25/Ejercicio-Clase/Ejercicio-Clase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio_Clase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número de 1 a 10");
+
+            int nGanador = 5;
+            int nElegido;
+
+            string val = Console.ReadLine();
+            nElegido = int.Parse(val);
+
+            while (nGanador!=nElegido)
+            {
+                Console.WriteLine("Ingrese otro número de 1 a 10");
+                nElegido = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acertaste!!");
+        }
+    }
+}
