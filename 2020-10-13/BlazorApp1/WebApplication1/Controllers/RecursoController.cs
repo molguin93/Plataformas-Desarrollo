af7d1b0e815e8931196713850d193cfe29de0969
MDarrell <mau_eo@hotmail.com>
2020-11-10 22:44:42 -0300

    Clase 10/11

---
+++ 2020-10-13/BlazorApp1/WebApplication1/Controllers/RecursoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using ClassLibrary1.Entidades;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecursoController : ControllerBase
+    {
+        private readonly TareasDbContext _context;
+
+        public RecursoController(TareasDbContext context)
+        {
+            _context = context;
+        }
+
+        //[HttpGet]
+        //public List<Recurso> Get()
+        //{
+        //    return 
+        //}
+    }
+}
